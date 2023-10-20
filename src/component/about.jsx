a7e37723feb6ba127cbe51b1d9a94de5424c58a9
charls <charlsmaritz@gmail.com>
2023-10-20 11:19:57 -0500

    (update: menu, header, resposive desing)

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -3,37 +3,44 @@ import { useInView } from 'react-intersection-observer';
 import { useEffect } from 'react';
 
 export default function About() {
+    const descriptions = [
+        "Hello, I'm Cristian, a passionate front-end developer with expertise in HTML, CSS, and JavaScript, including frameworks like React.",
+        "ImagePlaceholder",
+        "My focus is on delivering engaging and dynamic web experiences. Currently, I'm extending my skills to backend development."
+    ];
 
-    const { ref, inView, entry} = useInView({
-        /* Optional options */
-        threshold: 1,
+    const descriptionRefs = descriptions.map(() => {
+        return useInView({
+            threshold: 1,
+        });
     });
 
     useEffect(() => {
-        if (inView) {
-          // Si inView es true, muestra una alerta
-          entry.target.style.opacity = 1;
-          console.log(entry.target); 
-        }
-      }, [inView]);
+        descriptionRefs.forEach((descriptionRef, index) => {
+            const { ref, inView, entry } = descriptionRef;
+            if (inView) {
+                entry.target.style.opacity = 1;
+            }
+        });
+    }, [descriptionRefs]);
 
     return (
         <div className="about">
             <div className="about__description">
-
-                <div ref={ref} className="about__description--aside">
-                    Hello, I'm Cristian, a passionate front-end developer with expertise in HTML, CSS, and JavaScript, including frameworks like React. 
-                </div>
-                
-                <img  className='about__description--img' src="https://avatars.githubusercontent.com/u/61919822?v=4" alt="avatar from github" />
-
-                <div className="about__description--aside"> 
-                    My focus is on delivering engaging and dynamic web experiences, actually I'm extending my skills to backend development. 
-                </div>
+                {descriptions.map((text, index) => {
+                    if (index === 1) { // Render the image placeholder
+                        return (
+                            <img key={index} className='about__description--img' src="https://avatars.githubusercontent.com/u/61919822?v=4" alt="avatar from GitHub" />
+                        );
+                    } else { // Render text descriptions
+                        return (
+                            <div key={index} ref={descriptionRefs[index].ref} className="about__description--aside">
+                                {text}
+                            </div>
+                        );
+                    }
+                })}
             </div>
         </div>
-    )
-    
-
-        
+    )   
 }
