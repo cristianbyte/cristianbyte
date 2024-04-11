73b2ec9883cafc569109f1cf5bb2c7de26a86f82
cristianbyte <charlsmaritz@gmail.com>
2024-04-11 14:54:09 -0500

    about progress

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,19 @@
 import './about.css'
+import profile from '../assets/profile2.png'
 
 export default function About(){
     return (
         <div className="about">
-            <h2>about</h2>
+        <div className="about__me">
+            <h2 className='about__title'>
+                ABOUT ME
+            </h2>
+        </div>
+        <div className="about__photo">
+            <div className="photo--filter">
+                <img src={profile} alt="" srcset="" />
+            </div>
+        </div>
         </div>
     )
 }
