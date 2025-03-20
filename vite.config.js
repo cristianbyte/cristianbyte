3227d94fe7009c0010fdf548ca2f2565c0fa093e
cristianbyte <charlsmaritz@gmail.com>
2025-03-20 00:30:35 -0500

    vercel config for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cristianbyte",
+  base: process.env.NODE_ENV === 'production' && process.env.VERCEL 
+    ? '/' 
+    : '/cristianbyte',
 })
