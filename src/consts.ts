89e95687f9234ca5c1ebd87f5ccd3d0ee724e5e9
cristianbyte <charlsmaritz@gmail.com>
2025-04-26 17:13:42 -0500

    migrate: move project from React to Astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Coder.red';
+export const SITE_DESCRIPTION = 'Tendencias, ideas y futuro, desde la pasión por el código.';
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' && process.env.VERCEL 
-    ? '/' 
-    : '/cristianbyte',
+  base: '/',
 })
